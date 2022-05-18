9b9b09cc29e514b7bf026b271d7b7bc50d38dcee
Juan Bocanegra <jbocane6@gmail.com>
2022-05-17 22:19:02 -0500

    UPLOAD] task 1 v.1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+"""
+First you will setup a basic Flask app in 0-app.py.
+Creates a single / route and an index.html template
+that simply outputs “Welcome to Holberton” as page title(<title>)
+and “Hello world” as header (<h1>).
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Has LANGUAGES class attributes.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def test_page():
+    """
+    Creates a single / route and an index.html template.
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
