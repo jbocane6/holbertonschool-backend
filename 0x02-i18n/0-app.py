9ebafb9dab8af17a2dbe326963ace6d91edbe0b6
Juan Bocanegra <jbocane6@gmail.com>
2022-05-17 22:07:32 -0500

    UPLOAD] task 0 v.1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""
+First you will setup a basic Flask app in 0-app.py.
+Creates a single / route and an index.html template
+that simply outputs “Welcome to Holberton” as page title(<title>)
+and “Hello world” as header (<h1>).
+"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def zero():
+    """
+    Creates a single / route and an index.html template.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
